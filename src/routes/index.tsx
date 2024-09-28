db09845d202a4d0b32be28c96dbead0f7fc212f3
Dariusz Czajkowski <darek@darek.dev>
2024-09-28 17:31:51 +0200

    Some UI exploration, animating input

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,25 +1,12 @@
 import { createFileRoute } from '@tanstack/react-router'
+import { useCallback, useEffect, useRef, useState } from 'react'
+import { AnimatePresence, motion } from 'framer-motion'
+import { PaperAirplaneIcon } from '@heroicons/react/24/outline'
 
 export const Route = createFileRoute('/')({
   component: Index,
 })
 
 function Index() {
-  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault()
-
-    const formData = new FormData(e.currentTarget)
-    const prompt = formData.get('prompt') as string
-
-    console.log(prompt)
-  }
-
-  return (
-    <div className="flex items-center justify-center h-screen w-screen">
-      <form onSubmit={onSubmit}>
-        <input type="text" name="prompt" />
-        <button>submit</button>
-      </form>
-    </div>
-  )
+  return 'hi'
 }
