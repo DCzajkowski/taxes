cc8780c61b92bd4c34cdb9a1343562f7caa7d07a
Dariusz Czajkowski <darek@darek.dev>
2024-09-28 17:38:07 +0200

    Add shadcn

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,11 +2,12 @@ import { createFileRoute } from '@tanstack/react-router'
 import { useCallback, useEffect, useRef, useState } from 'react'
 import { AnimatePresence, motion } from 'framer-motion'
 import { PaperAirplaneIcon } from '@heroicons/react/24/outline'
+import { Button } from '@/components/ui/button'
 
 export const Route = createFileRoute('/')({
   component: Index,
 })
 
 function Index() {
-  return 'hi'
+  return <Button>some button</Button>
 }
